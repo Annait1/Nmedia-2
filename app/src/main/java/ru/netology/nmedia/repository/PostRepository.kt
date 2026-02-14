d6d11738a9654016df43ee43bd3f27b3399de747
Annait1 <email__github@example.com>
2026-02-14 04:54:50 +0300

    Migrate from thread to OkHttp enqueue

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -8,4 +8,36 @@ interface PostRepository {
     fun likeById(id: Long, likedByMe: Boolean): Post
     fun save(post: Post)
     fun removeById(id: Long)
+
+
+    fun likeByIdAsync(id: Long, likedByMe: Boolean, callback: LikeCallback)
+
+    fun saveAsync(post: Post, callback: SaveCallback)
+    fun removeByIdAsync(id: Long, callback: RemoveCallback)
+
+
+    fun getAllAsync(callback: GetAllCallback)
+
+
+    interface GetAllCallback {
+        fun onSuccess(posts: List<Post>) {}
+        fun onError(e: Exception) {}
+    }
+
+    interface LikeCallback {
+        fun onSuccess(post: Post) {}
+        fun onError(e: Exception) {}
+    }
+
+    interface SaveCallback {
+        fun onSuccess() {}
+        fun onError(e: Exception) {}
+    }
+
+    interface RemoveCallback {
+        fun onSuccess() {}
+        fun onError(e: Exception) {}
+    }
+
+
 }
